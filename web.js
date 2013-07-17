64b049db64799d2c00e0b25bc7c9d324747977aa
Ubuntu <ubuntu@ip-172-31-21-211.us-west-2.compute.internal>
2013-07-17 03:09:57 +0000

    restyled index.html and web.js

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs = require('fs'); 
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html');
-  var text = buf.toString();
-  response.send(text);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
